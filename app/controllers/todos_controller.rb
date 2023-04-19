71cf862182d41901949227f721a78b2706e8e135
brylenelavelle Patrick <brylenelavelle@gmail.com>
2023-04-19 15:11:25 +0000

    todos complete

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,60 @@
+class TodosController < ApplicationController
+  def index
+    matching_todos = Todo.all
+
+    @list_of_todos = matching_todos.order({ :created_at => :desc })
+
+    render({ :template => "todos/index.html.erb" })
+  end
+
+  def show
+    the_id = params.fetch("path_id")
+
+    matching_todos = Todo.where({ :id => the_id })
+
+    @the_todo = matching_todos.at(0)
+
+    render({ :template => "todos/show.html.erb" })
+  end
+
+  def create
+    the_todo = Todo.new
+    the_todo.content = params.fetch("query_content")
+    the_todo.status = "Next Up"
+
+    if the_todo.valid?
+      the_todo.save
+      redirect_to("/", { :notice => "Todo created successfully." })
+    else
+      redirect_to("/", { :alert => the_todo.errors.full_messages.to_sentence })
+    end
+  end
+
+  def update
+    the_id = params.fetch("path_id")
+    @the_todo = Todo.where({ :id => the_id }).at(0)
+
+    if @the_todo.status == "Next Up"
+      @the_todo.status = "In Progress"
+    elsif @the_todo.status == "In Progress"
+      @the_todo.status = "Done"
+    end
+
+    if @the_todo.valid?
+      @the_todo.save
+      redirect_to("/", { :notice => "Todo updated successfully."} )
+    else
+      redirect_to("/", { :alert => the_todo.errors.full_messages.to_sentence })
+    end
+
+  end
+
+  def destroy
+    the_id = params.fetch("path_id")
+    the_todo = Todo.where({ :id => the_id }).at(0)
+
+    the_todo.destroy
+
+    redirect_to("/", { :notice => "Todo deleted successfully."} )
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2023_04_18_191038) do
+
+  create_table "next_ups", force: :cascade do |t|
+    t.string "content"
+    t.string "status"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "todos", force: :cascade do |t|
+    t.string "content"
+    t.string "status"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "first_name"
+    t.string "last_name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
